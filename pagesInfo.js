d8bb2c41b52a618e399d953e408ed17453bdec9a
vanthan99 <vanthan.ad.it@gmail.com>
2023-03-19 11:57:50 +0700

    add page infor config and edit footer

---
+++ pagesInfo.js
@@ -0,0 +1,5 @@
+export const pagesInfo = {
+    name: 'phimnhanh.com',
+    title: 'Xem phim online miễn phí tốc độ nhanh trên Phimnhanh.com',
+    logo: 'http://demo.amytheme.com/movie/demo/elementor-movie-news/wp-content/uploads/sites/8/2022/05/img_65.png',
+}
