836cf122af7d595ab9016bdb588bf5e62887ae15
vanthan99 <vanthan.ad.it@gmail.com>
2023-05-08 00:38:25 +0700

    fix menu done

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { IMenu } from "@/commons/common";
 import { pageStyles } from "@/styles/styles.config";
-import { FacebookOutlined, InstagramOutlined, SkypeOutlined, TwitterOutlined, YoutubeOutlined } from "@ant-design/icons";
+import { CaretDownOutlined, FacebookOutlined, InstagramOutlined, SkypeOutlined, TwitterOutlined, YoutubeOutlined } from "@ant-design/icons";
 import { Space } from "antd";
 import Link from "next/link";
 import styled from "styled-components";
@@ -8,162 +8,162 @@ import Container from "../Container";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 const Menu = () => {
-    const MenuDatas : Array<IMenu> = [
+    const MenuDatas: Array<IMenu> = [
         {
             item: {
                 title: 'trang chủ',
-                link:'/'
+                link: '/'
             }
         },
         {
             item: {
                 title: 'thể loại',
-                link:'/the-loai',
-                parentLink:'/list',
+                link: '/the-loai',
+                parentLink: '/list',
             },
             subItems: [
                 {
                     title: 'phim hình sự',
-                    link:'/phim-hinh-su'
+                    link: '/phim-hinh-su'
                 },
                 {
                     title: 'phim tình cảm',
-                    link:'phim-tinh-cam'
+                    link: 'phim-tinh-cam'
                 },
                 {
                     title: 'phim hành động',
-                    link:'/phim-hanh-dong'
+                    link: '/phim-hanh-dong'
                 },
                 {
                     title: 'phim chiến tranh',
-                    link:'/phim-chien-tranh'
+                    link: '/phim-chien-tranh'
                 },
                 {
                     title: 'phim cổ trang',
-                    link:'/phim-co-trang'
+                    link: '/phim-co-trang'
                 },
                 {
                     title: 'phim bí ẩn',
-                    link:'/phim-bi-an'
+                    link: '/phim-bi-an'
                 },
                 {
                     title: 'phim võ thuật',
-                    link:'/phim-vo-thuat'
+                    link: '/phim-vo-thuat'
                 },
                 {
                     title: 'phim phiêu lưu',
-                    link:'/phim-phieu-luu'
+                    link: '/phim-phieu-luu'
                 },
                 {
                     title: 'phim tâm lý',
-                    link:'phim-tma-ly'
+                    link: 'phim-tma-ly'
                 },
                 {
                     title: 'phim kinh dị',
-                    link:'/phim-kinh-di'
+                    link: '/phim-kinh-di'
                 },
                 {
                     title: 'phim kịch tính',
-                    link:'/phim-kich-tinh'
+                    link: '/phim-kich-tinh'
                 },
                 {
                     title: 'phim hài',
-                    link:'/phim-hai'
+                    link: '/phim-hai'
                 },
                 {
                     title: 'phim viễn tưởng',
-                    link:'/phim-vien-tuong'
+                    link: '/phim-vien-tuong'
                 },
                 {
                     title: 'phim thần thoại',
-                    link:'phim-than-thoai'
+                    link: 'phim-than-thoai'
                 },
                 {
                     title: 'phim TV shows',
-                    link:'/phim-tv-shows'
+                    link: '/phim-tv-shows'
                 },
                 {
                     title: 'phim khoa học',
-                    link:'/phim-hoa-hoc'
+                    link: '/phim-hoa-hoc'
                 },
                 {
                     title: 'phim 18+',
-                    link:'/phim-18'
+                    link: '/phim-18'
                 },
                 {
                     title: 'phim gia đình',
-                    link:'/phim-gia-dinh'
+                    link: '/phim-gia-dinh'
                 },
             ]
         },
         {
             item: {
                 title: 'quốc gia',
-                link:'/quoc-gia',
-                parentLink:'/list',
+                link: '/quoc-gia',
+                parentLink: '/list',
             },
             subItems: [
                 {
                     title: 'phim hàn quốc',
-                    link:'/phim-han-quoc'
+                    link: '/phim-han-quoc'
                 },
                 {
                     title: 'phim mỹ',
-                    link:'/phim-my'
+                    link: '/phim-my'
                 },
                 {
                     title: 'phim nhật bản',
-                    link:'/phim-nhat-ban'
+                    link: '/phim-nhat-ban'
                 },
                 {
                     title: 'phim thái lan',
-                    link:'/phim-thai-lan'
+                    link: '/phim-thai-lan'
                 },
                 {
                     title: 'phim việt nam',
-                    link:'/phim-viet-nam'
+                    link: '/phim-viet-nam'
                 },
             ]
         },
         {
             item: {
                 title: 'phim mới',
-                link:'/phim-moi',
-                parentLink:'/list',
+                link: '/phim-moi',
+                parentLink: '/list',
             }
         },
         {
             item: {
                 title: 'phim bộ',
-                link:'/phim-bo',
-                parentLink:'/list',
+                link: '/phim-bo',
+                parentLink: '/list',
             }
         },
         {
             item: {
                 title: 'phim lẻ',
-                link:'/phim-le',
-                parentLink:'/list',
+                link: '/phim-le',
+                parentLink: '/list',
             }
         },
         {
             item: {
                 title: 'phim chiếu rạp',
-                link:'/phim-chieu-rap',
-                parentLink:'/list',
+                link: '/phim-chieu-rap',
+                parentLink: '/list',
             }
         },
     ];
     const router = useRouter();
-    const [activePage,setActivePage] = useState("/")
+    const [activePage, setActivePage] = useState("/")
     // useEffect(() => {
-        
+
     // },[])
     return (
         <MenuWrapper>
             <Container align='middle' style={{ height: '4rem' }}>
                 <Content>
-                   
+
                     <ul>
                         {
                             MenuDatas.map((value, index) => {
@@ -171,10 +171,10 @@ const Menu = () => {
                                 return (
                                     <li key={index}>
                                         <ParentItem>
-                                            {subItems != undefined && subItems.length > 0 && item.title || <StyledLink href={item.parentLink?item.parentLink+item.link:item.link}>{item.title}</StyledLink>}
+                                            {subItems != undefined && subItems.length > 0 && <>{item.title} <CaretDownOutlined style={{ fontSize: '14px' }} /></> || <StyledLink href={item.parentLink ? item.parentLink + item.link : item.link}>{item.title}</StyledLink>}
                                         </ParentItem>
                                         {subItems != undefined && subItems.length > 0 && <SubItemsWrapper>
-                                            {subItems?.map((value, i) => <SubItem key={i} href={item.parentLink?item.parentLink+value.link:value.link}>{value.title}</SubItem>)}
+                                            {subItems?.map((value, i) => <SubItem key={i} href={item.parentLink ? item.parentLink + value.link : value.link}>{value.title}</SubItem>)}
                                         </SubItemsWrapper>}
                                     </li>
                                 )
@@ -200,7 +200,6 @@ const Menu = () => {
                             </StyledLink>
                         </Space>
                     </Shared>
-
                 </Content>
             </Container>
         </MenuWrapper>
@@ -218,7 +217,7 @@ const ParentItem = styled.div<{ active?: 1 | 0 }>`
     }
 `
 
-    const StyledLink = styled(Link)`
+const StyledLink = styled(Link)`
     color: ${pageStyles.textColor};
     font-weight: 400;
     font-size: 17px;
@@ -241,12 +240,11 @@ const SubItem = styled(Link)`
 
 const SubItemsWrapper = styled.div`
     position: absolute;
-    display: flex;
+    display: none;
     width: 35rem;
     flex-wrap: wrap;
     justify-content: space-between;
     background-color: ${pageStyles.backgoundColor};
-    opacity: 0;
     transition: 0.5s;
     z-index: 1; 
     padding-top: 5px;
@@ -281,7 +279,7 @@ const MenuWrapper = styled.div`
                 
                 &:hover {
                     ${SubItemsWrapper} {
-                        opacity: 1
+                        display: flex;
                     }
                 }
             }
